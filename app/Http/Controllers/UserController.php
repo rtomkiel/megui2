b3b0d40cc4030229f391eb016502b4bf9cffeacb
rtomkiel <regis@doseextra.com>
2018-06-08 01:11:21 -0300

    User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Storage;
 use Request;
+use Imagick;
 
 
 class UserController extends Controller {
@@ -86,6 +88,7 @@ class UserController extends Controller {
 
         $id = $user;
 
+        $image = new Imagick();
 
         if (Request::get('password')) {
             $user->password = \Illuminate\Support\Facades\Hash::make(Request::get('password'));
@@ -99,13 +102,57 @@ class UserController extends Controller {
         
         if (Request::hasFile('image') && Request::file('image')->isValid())
         {
-
-                $name = md5(uniqid());
+                /*
+                 * Verifica a extensão da imagem
+                 */
                 $ext = $user->image->getClientOriginalExtension();
-                $path = "{$user->id}/$name.{$ext}";
-                $user->image = "$name.{$ext}";
-                $upload = $request->image->storeAs('users', $path);
-                
+
+
+                $originalName = $user->user . '_' . $user->id . '_original';
+                $originalFile = "{$user->id}/$originalName.{$ext}";
+
+                $thumbName = $user->user . '_' . $user->id . '_200x200';
+                $thumbFile = "{$user->id}/{$thumbName}";
+
+                $user->image = "$thumbName.jpg";
+                $upload = $request->image->storeAs('users', $originalFile);
+
+                /*
+                 * Compressão da imagem
+                 */
+                $image->readImage(storage_path() . '/app/public/' . $upload);
+
+                $w = $image->getImageWidth();
+                $h = $image->getImageHeight();
+
+                if($w > $h) {
+                    $resize_w = $w * 800/$h;
+                    $resize_h = 800;
+
+                    $image->cropImage(800,800, ($resize_w - 800), ($resize_h - 800));
+                }
+
+                $image->thumbnailImage(200,200);
+                $image->setCompressionQuality(75);
+                $image->stripImage();
+                $image->setFormat('jpeg');
+                $image->writeImage(storage_path() . '/app/public/users/' . $thumbFile . '.jpg');
+
+                /*
+                * Verifica o formato da imagem
+                */
+                if($ext == "png"){
+
+                $image->readImage(storage_path() . '/app/public/' . $upload);
+                $image->setFormat('jpeg');
+                $image->setCompressionQuality(75);
+                $image->writeImage(storage_path() . '/app/public/users/' . $user->id . '/' . $originalName . '.jpg');
+
+                Storage::delete('users/' . $originalFile);
+
+                }
+
+
                 if(!$upload) {
                     return redirect()->back()->with('error', 'Falha ao carregar imagem');
                 }
